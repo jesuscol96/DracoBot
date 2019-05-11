d3653e7105f0935f4afb155b3ec9796baabb7e88
14-10460 <38336339+14-10460@users.noreply.github.com>
2019-05-11 15:34:28 -0400

    Update draco_fkine.m

--- code/draco_fkine.m
+++ code/draco_fkine.m
@@ -1,7 +1,7 @@
 %This files describes Draco Bot Forward Kinetics
 
 clear; close all;
-
+x=0;
 %Lengths of the arm
 L1=1;
 L2=1;
@@ -9,6 +9,7 @@ L3=1;
 
 %Some useful positions
 qz = [0 0 0 0 0 0];
+qn = [0 0.7854 3.1416 0 0.7854 0 ];
 
 %Links definitions
 L(1)=Link([0 L1 0 pi/2],'R', 0);
@@ -20,3 +21,26 @@ L(6)=Link([0 0 0 -pi/2],'R', 0);
 
 %SerialLink Object
 draco=SerialLink(L,'name','Draco');
+
+
+
+while (1)
+    x=input('Bienvenidos a DracoBot,¿ Qué desea hacer?:\n 1 = Cinemática Directa\n 2 = Cinemática Inversa\n')
+    switch (x)
+        case (1)
+            fprintf('Cinemática Directa:')
+            draco.teach(qz)
+            x=0;
+        case (2)
+            fprintf('Cinemática Inversa\n:')
+            T=draco.fkine(qz)
+            t0 = inputdlg({'Ingrese coordenadas del punto deseado'},...
+                  'Cinemática Inversa', [1 50]); 
+            y = str2num(t0{1})
+            T.t=y'
+            qi = draco.ikine(T)
+            draco.teach(qi)
+            x=0;
+
+    end
+end
