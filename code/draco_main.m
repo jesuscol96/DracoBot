917657fc10c6d4c486cd3522fd2cd916d2b4b5cc
jesuscol96 <14-11384@usb.ve>
2019-05-11 22:05:08 -0400

    Order code

--- code/draco_main.m
+++ code/draco_main.m
@@ -1,29 +1,14 @@
 %This files describes Draco Bot Forward Kinetics
 
 clear; close all;
+
+%Running other files dependecies 
+draco_definitions;
+
+
+
+%Options for user to choose
 x=0;
-%Lengths of the arm
-L1=1;
-L2=1;
-L3=1;
-
-%Some useful positions
-qz = [0 0 0 0 0 0];
-qn = [0 0.7854 3.1416 0 0.7854 0 ];
-
-%Links definitions
-L(1)=Link([0 L1 0 pi/2],'R', 0);
-L(2)=Link([0 0 L2   0],'R', 0);
-L(3)=Link([0 0 0 pi/2],'R', 0);
-L(4)=Link([0 -L3 0 -pi/2],'R', 0);
-L(5)=Link([0 0 0 pi/2],'R', 0);
-L(6)=Link([0 0 0 -pi/2],'R', 0);
-
-%SerialLink Object
-draco=SerialLink(L,'name','Draco');
-
-
-
 while (1)
     x=input('Bienvenidos a DracoBot,¿ Qué desea hacer?:\n 1 = Cinemática Directa\n 2 = Cinemática Inversa R+T\n 3 = Cinemática Inversa T\n')
     switch (x)
