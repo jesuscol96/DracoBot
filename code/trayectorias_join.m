ca36b6ca074ecd60a73dc7375c9c548d565bbb12
14-10460 <38336339+14-10460@users.noreply.github.com>
2019-06-16 20:14:05 -0400

    Add files via upload

--- code/trayectorias_join.m
+++ code/trayectorias_join.m
@@ -1,24 +1,45 @@
 fprintf('Generador de Trayectorias:')
-prompt ={'Ingrese coordenadas del punto Inicial', 'Ingrese coordenadas del punto Final', 'Velocidad'};
+prompt ={'Ingrese coordenadas del punto Inicial', 'Angulo de Roll inicial', 'Angulo de Pitch inicial', 'Angulo de Yaw inicial' 'Ingrese coordenadas del punto Final','Angulo de Roll final', 'Angulo de Pitch final', 'Angulo de Yaw final', 'Tiempo Maximo', 'Tiempo de Muestreo'};
 dlgtitle ='Generador de Trayectorias:';
 dims=[1 50];
-definput = {'0 0 0','0.5 0.5 0.5','0.1'}; 
+definput = {'0 0 0','0','0','0','0.5 0.5 0.5','pi/2','pi/2','pi/2','5','0.1'}; 
 u = inputdlg(prompt,dlgtitle,dims,definput)
 y = u(1);
 y = str2num(y{1})
 xi=y
 y = u(2);
 y = str2num(y{1})
-xf = y
+qri=y
 y = u(3);
 y = str2num(y{1})
-v = y
-t = [0:v:5]';
-Ti = SE3(xi)
-Tf = SE3(xf)
+qpi=y
+y = u(4);
+y = str2num(y{1})
+qyi=y
+y = u(5);
+y = str2num(y{1})
+xf = y
+y = u(6);
+y = str2num(y{1})
+qrf=y
+y = u(7);
+y = str2num(y{1})
+qpf=y
+y = u(8);
+y = str2num(y{1})
+qyf=y
+y = u(9);
+y = str2num(y{1})
+tmax = y
+y = u(10);
+y = str2num(y{1})
+tsam = y
+t = [0:tsam:tmax]';
+Ti = SE3(xi) * SE3.Rz(qri)*SE3.Ry(qpi)*SE3.Rx(qyi);
+Tf = SE3(xf) * SE3.Rz(qrf)*SE3.Ry(qpf)*SE3.Rx(qyf);
 qi = draco.ikine(Ti);
 qf = draco.ikine(Tf);
-[q,qd,qdd] = jtraj(qi, qf, t);
+q = mtraj(@tpoly,qi, qf, t);
 figure(1);
 draco.teach(q)
 figure(2);
@@ -30,10 +51,10 @@ p = T.transl
 subplot(2,2,2);
 plot(t, p(:,1),'b-',t, p(:,2),'r-',t, p(:,3),'g-')
 legend('x','y','z')
-xlabel('Time (s)'), ylabel('Position (m)')
+xlabel('Tiempo (s)'), ylabel('Posicion (m)')
 subplot(2,2,3);
 plot(p(1,:), p(2,:))
 xlabel('x(m)'), ylabel('y(m)')
 subplot(2,2,4);
 plot(t, T.torpy('xyz'))
-xlabel('Time (s)'), ylabel('RPY angles(rad)')
+xlabel('Tiempo (s)'), ylabel('Angulos de RPY(rad)')
